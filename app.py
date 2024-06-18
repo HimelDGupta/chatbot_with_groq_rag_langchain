ce58de5e45300367e67733af4384779142050fbc
Das <Himel.Das@ttu.edu>
2024-06-18 14:19:54 -0500

    Importing dependency and streamlit configure

---
+++ app.py
@@ -0,0 +1,25 @@
+import streamlit as st
+import os
+from langchain_groq import ChatGroq
+from langchain_community.document_loaders import WebBaseLoader
+from langchain_community.embeddings import OllamaEmbeddings
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_core.prompts import ChatPromptTemplate
+from langchain.chains import create_retrieval_chain
+
+from dotenv import load_dotenv
+load_dotenv()
+
+## Load the groq api key
+groq_api_key = os.environ['GROQ_API_KEY']
+
+
+if "vector" not in st.session_state:
+    st.session_state.embedding = OllamaEmbeddings()
+    st.session_state.loader = WebBaseLoader("https://docs.smith.langchain.com/")
+    st.session_state.docs = st.session_state.loader.load()
+
+    st.session_state.text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+    st.session_state.final_documents = st.session_state.text_splitter.split_documents(st.session_state.docs, st.session_state.embeddings)
+    
